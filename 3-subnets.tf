d2ea8d95eda2e604bd436b181917ad85aa84a3a7
Bunvicky <victoriaolawal05@gmail.com>
2023-12-13 14:34:14 -0500

    update 2

--- 3-subnets.tf
+++ 3-subnets.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "private-us-east-1a" {
+resource "aws_subnet" "private-us-east-2a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.0.0/19"
   availability_zone = "us-east-2a"
@@ -10,7 +10,7 @@ resource "aws_subnet" "private-us-east-1a" {
   }
 }
 
-resource "aws_subnet" "private-us-east-1b" {
+resource "aws_subnet" "private-us-east-2b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.32.0/19"
   availability_zone = "us-east-2b"
