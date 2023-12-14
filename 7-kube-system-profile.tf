6b66fa7bf29824421712995a96b403a9e3dedf53
Bunvicky <victoriaolawal05@gmail.com>
2023-12-14 09:15:49 -0500

    ingress

--- 7-kube-system-profile.tf
+++ 7-kube-system-profile.tf
@@ -108,3 +108,19 @@ resource "aws_eks_fargate_profile" "argocd" {
     namespace = "argocd"
   }
 }
+resource "aws_eks_fargate_profile" "argocd" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "ingress-nginx"
+  pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
+
+  # These subnets must have the following resource tag: 
+  # kubernetes.io/cluster/<CLUSTER_NAME>.
+  subnet_ids = [
+    aws_subnet.private-us-east-1a.id,
+    aws_subnet.private-us-east-1b.id
+  ]
+
+  selector {
+    namespace = "ingress-nginx"
+  }
+}
